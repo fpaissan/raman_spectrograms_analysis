648292838bbb3a55f436161634fbd28414c220a3
Francesco Paissan <me@francescopaissan.it>
2021-06-13 10:34:59 +0200

    Add k-medoids w/ different metrics in train_model.py

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,20 +1,24 @@
-import pickle
-
-from sklearn.cluster import KMeans
+# Written by Francesco Paissan
+from scipy.spatial.distance import cosine, correlation
+from sklearn_extra.cluster import KMedoids
 from progress.bar import ShadyBar
 import numpy as np
 import logging
+import pickle
 import click
 import glob
 
-from src.models.utils import load_data
 
+def train_model(data_x, metric, n_clusters=60):
+    """ Train model_type on data_x using metric. """
+    metrics_lambdas = {
+        "1-norm": lambda x, y: np.linalg.norm(x - y, ord=1),
+        "2-norm": lambda x, y: np.linalg.norm(x - y, ord=2),
+        "cosine": lambda x, y: cosine(x, y),
+        "correlation": lambda x, y: correlation(x, y),
+    }
 
-def train_model(data_x, n_clusters=8):
-    """
-    Finds k-means centroids
-    """
-    k_mean = KMeans(n_clusters=n_clusters)  # perche 61?
+    k_mean = KMedoids(n_clusters=n_clusters, metric=metrics_lambdas[metric])
     k_mean.fit(data_x)
 
     return k_mean
@@ -22,22 +26,20 @@ def train_model(data_x, n_clusters=8):
 
 @click.command()
 @click.argument('feature_filepath', type=click.Path(exists=True))
-def main(feature_filepath):
+@click.argument('metric', type=str)
+def main(feature_filepath, metric):
     file_list = glob.glob('{0}/*'.format(feature_filepath))
 
-    r_start = 0
-    r_end = 1400
-
-    # TODO: Check if 1D is ok
     with ShadyBar(f"Loading dataset...", max=len(file_list)) as bar:
         for f in file_list:
-            if not "bkg" in f:
-                data_x = load_data(f)
+            with open(f, "rb") as f:
+                data_x = pickle.load(f)
+
             bar.next()
 
-    k_mean = train_model(data_x)
+    k_mean = train_model(data_x, metric)
 
-    with open(f"models/k_means_range.pkl", 'wb') as f:
+    with open(f"models/trained_model.pkl", 'wb') as f:
         pickle.dump(k_mean, f)
 
     return k_mean
